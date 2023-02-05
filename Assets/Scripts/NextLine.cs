c1131f19fe8c211d0bdb24e0c7feb6cbd94cc7ab
NiceNameNicky <fantasticnicky@gmail.com>
2023-02-05 04:57:08 -0500

    Cutscenes and music

---
+++ Assets/Scripts/NextLine.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLine : MonoBehaviour
+{
+    public int dialogueIndex = 0;
+    public GameObject[] Dialogues;
+    public bool Ending;
+
+    public void NextDialogue()
+    {
+        for (int i = 0; i < Dialogues.Length; i++)
+        {
+            if(i == dialogueIndex)
+            {
+                Dialogues[i].SetActive(true);
+            } else
+            {
+                Dialogues[i].SetActive(false);
+            }
+        }
+
+
+        if (dialogueIndex >= Dialogues.Length)
+        {
+            if (Ending)
+            {
+                SceneManager.LoadScene("Main Menu");
+            } else 
+            { 
+                SceneManager.LoadScene("SampleScene"); 
+            }
+                
+                
+        }
+
+
+        dialogueIndex++;
+    }
+}

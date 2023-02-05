8d63ddee1f2526dab3f3b3ae45c0c1ade50e008d
NiceNameNicky <fantasticnicky@gmail.com>
2023-02-05 15:46:17 -0500

    Playable Loop

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public GameObject[] Slides;
+    int slideIndex = 0;
+    float timeCounter = 4f;
+
+    private void Start()
+    {
+        Slides[slideIndex].SetActive(true);
+    }
+
+    public void Update()
+    {
+        timeCounter -= Time.deltaTime;
+
+        if(timeCounter < 0)
+        {
+            Slides[slideIndex].SetActive(false); 
+            slideIndex++;
+            timeCounter = 4f;
+            Slides[slideIndex].SetActive(true);
+        }
+
+        if(slideIndex >= Slides.Length)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

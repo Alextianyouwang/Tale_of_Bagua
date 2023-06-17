3458c85609486fe91710195a8d9132413948d9cb
Cecil Boey <cecil_12345678@yahoo.com>
2023-06-17 17:06:48 +0800

    06.17.2023 - LevelArtRefined

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour
 {
-    public Collider[] boxs;
+    public Collider[] boxs, boxsOct;
     bool colliderToggleState = false;
     void Start()
     {
@@ -13,6 +13,15 @@ public class Mirror : MonoBehaviour
     {
         boxs = GetComponents<Collider>();
         foreach (Collider b in boxs)
+            b.isTrigger = true;
+
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponentInChildren<MeshCollider>() != null)
+                boxsOct = GetComponentsInChildren<MeshCollider>();
+        }
+
+        foreach (Collider b in boxsOct)
             b.isTrigger = true;
     }
    

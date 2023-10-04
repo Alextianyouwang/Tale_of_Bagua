14cd5da07ad41dd586b717dc007e1c78488b2b18
Alex Wang <54587384+Alextianyouwang@users.noreply.github.com>
2023-10-04 01:00:33 -0700

    PlaneLevelCreator_10/04/2023

--- Assets/Scripts/Optimization/CaptureLevel.cs
+++ Assets/Scripts/Optimization/CaptureLevel.cs
@@ -8,6 +8,8 @@ public class CaptureLevel
 {
     public Camera Cam { get; set; }
 
+    
+
     public void CaptureCameraToTexture(LayerMask cullMask, string path, string name)
     {
         if (Cam == null)
@@ -36,6 +38,49 @@ public class CaptureLevel
 
         AssetDatabase.Refresh();
 
+    }
+
+    public Vector3[] GetScreenInWorldSpace(float depth) 
+    {
+        Vector3[] corners = new Vector3[4];
+        if (Cam == null)
+        {
+            Debug.LogError("Camera reference is null!");
+            return corners;
+        }
+        corners[0] = Cam.ScreenToWorldPoint(new Vector3(0, 0, depth));
+        corners[1] = Cam.ScreenToWorldPoint(new Vector3(Cam.pixelWidth, 0, depth));
+        corners[2] = Cam.ScreenToWorldPoint(new Vector3(Cam.pixelWidth, Cam.pixelHeight, depth));
+        corners[3] = Cam.ScreenToWorldPoint(new Vector3(0, Cam.pixelHeight, depth));
+        return corners;
+    }
+
+    public GameObject CreatePlaneLevel(string name, Mesh mesh, Material mat) 
+    {
+        GameObject g = new GameObject(name);
+         MeshFilter mf = g.AddComponent<MeshFilter>();
+         MeshRenderer mr = g.AddComponent<MeshRenderer>();
+        mr.sharedMaterial = mat;
+        mf.mesh = mesh;
+        return g;
+    }
+
+    public void AdjustQuadDepth(Mesh target,float depth) 
+    {
+        target.vertices = GetScreenInWorldSpace(depth);
+    }
+    public Mesh CreateQuad(Vector3[] corners) 
+    {
+       
+        Mesh m = new Mesh();
+
+        m.vertices = new Vector3[] { corners[0], corners[1], corners[2], corners[3] };
+        m.triangles = new int[] { 0, 3, 2, 2, 1, 0 };
+
+        m.RecalculateNormals();
+        m.RecalculateBounds();
+
+        return m;
     }
 
 }
--- Assets/Scripts/Optimization/CaptureLevel_Editor.cs
+++ Assets/Scripts/Optimization/CaptureLevel_Editor.cs
@@ -8,6 +8,11 @@ public class SceneCaptureWindow : EditorWindow
     private string _path;
     private string _name;
     private LayerMask _mask;
+
+    private GameObject _levelObj;
+    private Material _levelMat;
+    private Mesh _levelMesh;
+    private float _levelDepth = 1f, _prev_levelDepth;
     [MenuItem("Tool/Scene Capture")]
     public static void ShowWindow()
     {
@@ -18,15 +23,22 @@ public class SceneCaptureWindow : EditorWindow
     private void OnEnable()
     {
         _master = new CaptureLevel();
+        _master.Cam = FindObjectOfType<Camera>();
     }
     private void OnDisable() 
     {
     
     }
+    public static void DrawSeparator()
+    {
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+    }
+    
     private void OnGUI()
     {
         EditorGUILayout.LabelField("Scene Capture", EditorStyles.boldLabel);
-        _master.Cam = (Camera)EditorGUILayout.ObjectField("Camera", _master.Cam, typeof(Camera), true);
+       
+        _master.Cam = (Camera)EditorGUILayout.ObjectField("Camera", _master.Cam , typeof(Camera), true);
         if (!_master.Cam)
             return;
 
@@ -38,5 +50,21 @@ public class SceneCaptureWindow : EditorWindow
         {
             _master.CaptureCameraToTexture(_mask << 1,_path, _name);
         }
+
+        DrawSeparator();
+        _levelDepth = EditorGUILayout.Slider("Level Depth",_levelDepth, _master.Cam.nearClipPlane, _master.Cam.farClipPlane);
+        _levelMat = (Material)EditorGUILayout.ObjectField("Level Material",_levelMat, typeof(Material),true);
+
+        
+        if (GUILayout.Button("Create Level From Camera View"))
+        {
+            if (_levelObj)
+                DestroyImmediate(_levelObj);
+            _levelMesh = _master.CreateQuad(_master.GetScreenInWorldSpace(_levelDepth));
+            _levelObj = _master.CreatePlaneLevel("Test",_levelMesh ,_levelMat);
+        }
+        if (_prev_levelDepth != _levelDepth && _levelObj != null)
+            _master.AdjustQuadDepth(_levelMesh, _levelDepth);
+        _prev_levelDepth = _levelDepth;
     }
 }

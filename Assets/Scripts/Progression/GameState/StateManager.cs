e41fc291d948fa4bcb237941ce9313561ac2e19f
Alex Wang <54587384+Alextianyouwang@users.noreply.github.com>
2024-01-25 20:17:20 -0800

    Add_BasicFlowModule_01/25/2024

--- Assets/Scripts/Progression/GameState/StateManager.cs
+++ Assets/Scripts/Progression/GameState/StateManager.cs
@@ -1,12 +1,13 @@
 
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class StateManager : MonoBehaviour
 {
     public static StateManager instance;
     [SerializeField]
-    private SceneDataObject[] sceneDatas;
+    private SceneDataObject[] _sceneDatas;
+    private int _currentScene = 0;
     private void Awake()
     {
         CreateSingleton();
@@ -14,11 +15,53 @@ public class StateManager : MonoBehaviour
 
     void CreateSingleton() 
     {
-        if (instance != null)
+        if (instance == null)
             instance = this;
         else
-            Destroy(this);
-        DontDestroyOnLoad(this); 
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    public void NextScene() 
+    {
+        if (_sceneDatas.Length == 0) 
+        {
+            Debug.LogWarning("No Scene Data In Slot");
+            return;
+        }
+        if (_currentScene >= _sceneDatas.Length - 1) 
+        {
+            Debug.LogWarning("All Scenes Have Been Played Through");
+            return;
+        }
+           
+        _currentScene++;
+        LoadScene(_sceneDatas[_currentScene].Name);
+    }
+    public void PreviousScene() 
+    {
+        if (_sceneDatas.Length == 0)
+        {
+            Debug.LogWarning("No Scene Data In Slot");
+            return;
+        }
+        if (_currentScene <= 0)
+        {
+            Debug.LogWarning("This Is The Very First Scene");
+            return;
+        }
+        _currentScene--;
+        LoadScene(_sceneDatas[_currentScene].Name);
+    }
+
+    private void LoadScene(string name) 
+    {
+        if (!Application.CanStreamedLevelBeLoaded(name) )
+        {
+            Debug.LogWarning($"Scene at Index {_currentScene} has invalid name");
+            return;
+        }
+            SceneManager.LoadScene(name,LoadSceneMode.Single);
     }
     void Start()
     {
